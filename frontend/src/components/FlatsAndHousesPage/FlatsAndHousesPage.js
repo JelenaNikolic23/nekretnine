a6368f01b03355ca0985b04be9d21c7116472b26
JelenaNikolic23 <nikolic.23jelena@gmail.com>
2022-08-05 11:45:55 +0200

    izdvajanje zajednicke komponente

--- frontend/src/components/FlatsAndHousesPage/FlatsAndHousesPage.js
+++ frontend/src/components/FlatsAndHousesPage/FlatsAndHousesPage.js
@@ -1,33 +1,17 @@
+import PageIntro from "../Common/PageIntro";
 import HousesAndFlatsList from "./HousesAndFlatsList";
 
 function FlatsAndHousesPage() {
+
+    const pageIntroProps = {
+        title: "Kuce i stanovi",
+        subtitle: "Stambeni objekti",
+        pathLabel: "Kuce i stanovi"
+    }
+
     return (
         <>
-            <section className="intro-single">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12 col-lg-8">
-                            <div className="title-single-box text-start">
-                                <h1 className="title-single text-start">Kuce i stanovi</h1>
-                                <span className="color-text-a text-start">Stambeni objekti</span>
-                            </div>
-                        </div>
-                        <div className="col-md-12 col-lg-4">
-                            <nav aria-label="breadcrumb" className="breadcrumb-box d-flex justify-content-lg-end">
-                                <ol className="breadcrumb">
-                                    <li className="breadcrumb-item">
-                                        <span>Pocetna</span>
-                                    </li>
-                                    <li className="breadcrumb-item active" aria-current="page">
-                                        Kuce i stanovi
-                                    </li>
-                                </ol>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
+            <PageIntro data={pageIntroProps} />
             <HousesAndFlatsList />
         </>
     )
--- frontend/src/components/LocalsPage/LocalsPage.js
+++ frontend/src/components/LocalsPage/LocalsPage.js
@@ -1,34 +1,18 @@
+import PageIntro from "../Common/PageIntro";
 import LocalsList from "./LocalsList";
 
 
 function LocalsPage() {
+
+    const pageIntroProps = {
+        title: "Lokali",
+        subtitle: "Poslovni objekti",
+        pathLabel: "Lokali"
+    }
+
     return (
         <>
-            <section className="intro-single">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12 col-lg-8">
-                            <div className="title-single-box text-start">
-                                <h1 className="title-single text-start">Lokali</h1>
-                                <span className="color-text-a text-start">Poslovni objekti</span>
-                            </div>
-                        </div>
-                        <div className="col-md-12 col-lg-4">
-                            <nav aria-label="breadcrumb" className="breadcrumb-box d-flex justify-content-lg-end">
-                                <ol className="breadcrumb">
-                                    <li className="breadcrumb-item">
-                                        <span>Pocetna</span>
-                                    </li>
-                                    <li className="breadcrumb-item active" aria-current="page">
-                                        Lokali
-                                    </li>
-                                </ol>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
+            <PageIntro data={pageIntroProps} />
             <LocalsList />
         </>
     )
