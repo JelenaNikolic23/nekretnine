3c97d8fe639a898f5a51564fe2e68069f60042c9
Jelena B. Nikolic <jelena.b.nikolic@yettel.rs>
2022-09-13 15:21:15 +0200

    Add apartments

--- frontend/src/components/HomesPage/HomesPage.js
+++ frontend/src/components/HomesPage/HomesPage.js
@@ -5,56 +5,53 @@ import { getHomes } from "../../services/RealEstateService";
 import Loader from "../Common/Loader";
 import { hideLoader } from "../../utils";
 
-
 function HomesPage() {
+  const pageIntroProps = {
+    title: "Kuce i stanovi",
+    subtitle: "Stambeni objekti",
+    pathLabel: "Kuce i stanovi",
+  };
 
-    const pageIntroProps = {
-        title: "Kuce i stanovi",
-        subtitle: "Stambeni objekti",
-        pathLabel: "Kuce i stanovi"
-    }
+  const [items, setItems] = useState([]);
 
-    const [items, setItems] = useState([]);
+  useEffect(() => {
+    getHomes().then((response) => {
+      if (response.status !== 200) {
+      }
 
-    useEffect(() => {
-        getHomes().then(response => {
-            if (response.status !== 200) {
+      const flatsAndHouses = response.data.data.map((item) => {
+        let imageUrl = "";
+        if (item.attributes.pictures.data.length > 0) {
+          imageUrl = item.attributes.pictures.data[0].attributes.url;
+        }
 
-            }
+        return {
+          id: item.id,
+          title: item.attributes.title,
+          location: item.attributes.location,
+          city: item.attributes.city.data.attributes.name,
+          rooms: item.attributes.rooms,
+          bathrooms: item.attributes.bathrooms,
+          garages: item.attributes.garages,
+          surface: item.attributes.surface,
+          price: item.attributes.price,
+          imageUrl: "http://localhost:1337" + imageUrl,
+          overviewLink: "/homes/" + item.id,
+        };
+      });
 
-            const flatsAndHouses = response.data.data.map(item => {
-                let imageUrl = "";
-                if (item.attributes.pictures.data.length > 0) {
-                    imageUrl = item.attributes.pictures.data[0].attributes.url;
-                }
+      setItems(flatsAndHouses);
+      hideLoader();
+    });
+  }, []);
 
-                return {
-                    id: item.id,
-                    title: item.attributes.title,
-                    location: item.attributes.location,
-                    city: item.attributes.city.data.attributes.name,
-                    rooms: item.attributes.rooms,
-                    bathrooms: item.attributes.bathrooms,
-                    garages: item.attributes.garages,
-                    surface: item.attributes.surface,
-                    price: item.attributes.price,
-                    imageUrl: "http://localhost:1337" + imageUrl,
-                    overviewLink: "/homes/" + item.id,
-                }
-            });
-
-            setItems(flatsAndHouses);
-            hideLoader();
-        });
-    }, []);
-
-    return (
-        <>
-            <PageIntro data={pageIntroProps} />
-            <HomesList items={items} />
-            <Loader />
-        </>
-    )
+  return (
+    <>
+      <PageIntro data={pageIntroProps} />
+      <HomesList items={items} />
+      <Loader />
+    </>
+  );
 }
 
 export default HomesPage;
